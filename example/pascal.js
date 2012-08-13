d6c83297740025e7cfed3b16647d7ea2e8ac71ff
James Halliday <mail@substack.net>
2012-08-13 16:59:14 -0700

    prettier triangle

--- example/pascal.js
+++ example/pascal.js
@@ -1,10 +1,17 @@
 var choose = require('../');
 
-var rows = 15;
-for (var n = 0; n < rows; n++) {
+var count = Number(process.argv[2]) || 20;
+var rows = [];
+for (var n = 0; n < count; n++) {
     var row = [];
     for (var k = 0; k <= n; k++) {
         row.push(choose(n, k));
     }
-    console.dir(row);
+    rows.push(row.join(' '));
 }
+
+var len = rows[rows.length - 1].length;
+rows.forEach(function (row) {
+    var n = Math.floor((len - row.length) / 2) + 1;
+    console.log(Array(n).join(' ') + row);
+});
